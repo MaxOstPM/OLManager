0c9837aab1dcc62755c9dff5e11231a458196d2c
Максим Остапчук <kurtmo17@gmail.com>
2019-02-12 15:59:16 +0200

    Initial commit

---
+++ Example/Pods/Target Support Files/OLManager/OLManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_OLManager : NSObject
+@end
+@implementation PodsDummy_OLManager
+@end
---
+++ Example/Pods/Target Support Files/Pods-OLManager_Tests/Pods-OLManager_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OLManager_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_OLManager_Tests
+@end
