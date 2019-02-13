7dfb2c29a97d0a79db61a4697c9da6639faaf9da
Максим Остапчук <kurtmo17@gmail.com>
2019-02-13 12:46:41 +0200

    Added missed file

---
+++ OLManager/Classes/ServiceLayer/OverlayManagerContract.swift
@@ -0,0 +1,24 @@
+//
+//  OverlayContract.swift
+//  ViewsManager
+//
+//  Created by Максим Остапчук on 1/25/19.
+//  Copyright © 2019 Максим Остапчук. All rights reserved.
+//
+
+import UIKit
+
+enum Edge {
+    case top
+    case bottom
+    // TODO: add left and right edges
+}
+
+// MARK: OverlayManager
+protocol ViewControllerObservable {
+    func viewControllerBecomeActive(_ viewController: UIViewController)
+}
+
+protocol OverlayRemovable: class {
+    func removeOverlay(_ overlay: UIView, animated: Bool)
+}
