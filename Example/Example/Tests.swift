059bb9ea60655d9e441494e7cc2cc14bd663687c
Максим Остапчук <kurtmo17@gmail.com>
2019-04-25 12:00:10 +0300

    Fix Example

--- Example/Example/Tests.swift
+++ Example/Example/Tests.swift
@@ -1,55 +1,78 @@
 import OLManager
+import UIKit
 
-enum Overlays: OverlayLevelHolder {
-    case provideStatistic
-    case paidSocialFailure
+// MARK: - OverlayFlowSetup
+enum OverlayViewType: OverlayLevelHolder {
     
     var level: OverlayLevel {
-        switch self {
-        case .provideStatistic:
-            return .local
-        case .paidSocialFailure:
-            return .local
-        }
+        return .local
     }
+    
+    case greenTest
+    case blackTest
+    case redTest
 }
 
 final class OverlayFactoryImp: OverlayFactory {
     
-    func makeOverlayWith(type: Overlays) -> UIView {
-        return UIView()
+    typealias EnumType = OverlayViewType
+    
+    func makeOverlayWith(type: OverlayViewType) -> UIView {
+        let testOverlay: UIView = UIView()
+        
+        var color: UIColor
+        
+        switch type {
+        case .greenTest:
+            color = .green
+            
+        case .blackTest:
+            color = .black
+            
+        case .redTest:
+            color = .red
+        }
+        
+        testOverlay.backgroundColor = color.withAlphaComponent(0.5)
+        return testOverlay
     }
 }
 
 final class OverlayManagerProvider {
     
     static let provider = OverlayManagerProvider()
-    
     var overlayManager: OverlayManagerOf<OverlayFactoryImp>
     
     init() {
         let factory = OverlayFactoryImp()
-        overlayManager = OverlayManagerOf<OverlayFactoryImp>(factory: factory)
+        self.overlayManager = OverlayManagerOf<OverlayFactoryImp>(factory: factory)
     }
 }
 
-final class TestClass: UIViewController {
+// MARK: - TestViewController
+final class ViewController: UIViewController {
     
     let overlayManager = OverlayManagerProvider.provider.overlayManager
+    var overlayManageable: OverlayManageable?
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let location = PinningLocation(verticalEdge: .top)
-        let configuration = OverlayDisplayConfiguration(animationType: .none, pinningLocation: location)
-        _ = overlayManager.displayOverlay(.paidSocialFailure, configuration: configuration)
-    }
-    
-    @objc override var overlayInsets: UIEdgeInsets {
+    override var localOverlayInsets: UIEdgeInsets {
         let navBarY = self.navigationController?.navigationBar.frame.origin.y ?? 0.0
         let navBarHeight = self.navigationController?.navigationBar.frame.size.height ?? 0.0
         let tabBarHeight = self.tabBarController?.tabBar.frame.size.height ?? 0.0
         
-        return UIEdgeInsets(top: navBarY + navBarHeight, left: 0.0, bottom: tabBarHeight, right: 0.0)
+        return UIEdgeInsets(top: navBarY + navBarHeight * 2, left: 0.0, bottom: tabBarHeight, right: 0.0)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        overlayManager.viewControllerBecomeActive(self)
+    }
+    
+    // MARK: - UserInteraction
+    @IBAction func testButtonAction(_ sender: Any) {
+        let location = PinningLocation(horizontalEdge: .left, verticalEdge: .top)
+        let config = OverlayDisplayConfiguration(animationType: .slide, pinningLocation: location)
+        overlayManageable = overlayManager.displayOverlay(OverlayViewType.greenTest, configuration: config)
     }
 }
